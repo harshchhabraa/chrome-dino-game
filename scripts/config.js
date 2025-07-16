4bc4df61a798785dfd5cb1db12464e2c1642a71a
harshchhabraa <harshchhabra0512@gmail.com>
2025-07-16 22:47:02 +0530

    Add files via upload

---
+++ scripts/config.js
@@ -0,0 +1,7 @@
+export const GAME_WIDTH = 900;
+export const GAME_HEIGHT = 300;
+export const FRAME_RATE = 70;
+export const SPEED = 20;
+export const MIN = 600;
+export const MAX = 1800;
+export const GRAVITY = 10;
